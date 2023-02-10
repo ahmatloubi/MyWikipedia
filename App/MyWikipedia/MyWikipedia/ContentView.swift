8bb87391c3ce3831eb24a4b4769d82eeebf4a659
AmirHossein Matloubi <a.h.matloubi@gmail.com>
2023-02-10 18:11:59 +0330

    Add Project

---
+++ App/MyWikipedia/MyWikipedia/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyWikipedia
+//
+//  Created by AmirHossein Matloubi on 11/21/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ App/MyWikipedia/MyWikipedia/MyWikipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWikipediaApp.swift
+//  MyWikipedia
+//
+//  Created by AmirHossein Matloubi on 11/21/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct MyWikipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
